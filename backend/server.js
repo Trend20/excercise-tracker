9b9a9e9ec654e27563b9dfa89df471feacd7b5db
DevEnock <enockomondi305@gmail.com>
2022-01-27 06:54:09 +0300

    updating backend code

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const cors = require('cors');
+
 const mongoose = require('mongoose');
 
+
 const app = express();
+
+require('dotenv').config();
 
 // use middlewares
 app.use(cors());
 app.use(express.json());
 
+// connect to the database
+const url = process.env.DATABASE_URL;
+mongoose.connect(url, {useNewUrlPaser: true, useCreateIndex: true});
+
+// declare connection
+const connection = mongoose.connection;
+
+connection.once('open', () =>{
+  console.log('Application is connected to the database successfully!');
+})
+
 // declare the PORT
 const PORT = process.env.PORT || 5500;
 
 // listen to the port
-app.listen(() =>{
+app.listen(PORT, () =>{
   console.log(`Application is listening on port ${PORT}`);
-})
+});
