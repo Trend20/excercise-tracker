098045a58b55968d54ea119419d56754c995104c
DevEnock <enockomondi305@gmail.com>
2022-01-31 16:09:40 +0300

    adding exercise components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <Router>
       <div className="container">
-        <h1>Tracker app</h1>
+        <Navbar />
+        <Route path="" exact component={ExercisesList}/>
+        <Route path="/edit/:id" component={EditExercise} />
+        <Route path="/create" component={CreateExercise} />
+        <Route path="/user" component={CreateUser} />
       </div>
     </Router>
   );
---
+++ src/components/ExercisesList/ExercisesList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './ExercisesList.css'
+
+function ExercisesList() {
+  return <div></div>;
+}
+
+export default ExercisesList;
