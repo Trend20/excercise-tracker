c3f76f173452d10e45ebe331ebe399e3a3381c44
DevEnock <enockomondi305@gmail.com>
2022-02-02 15:35:04 +0300

    adding the cerate user component

--- src/components/CreateUser/CreateUser.js
+++ src/components/CreateUser/CreateUser.js
@@ -1,6 +1,34 @@
 import React, { Component } from 'react';
 
 class CreateUser extends Component {
+
+  constructor(props){
+    super(props)
+    this.state ={
+      username: ''
+    }
+  }
+
+  onChangeUsername = (event) =>{
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  onSubmitUser = (event) =>{
+
+    event.preventDefault();
+
+    const newUser = {
+      username: this.state.username
+    }
+
+    console.log(newUser);
+
+    this.setState({
+      username: ''
+    })
+  }
   render() {
     return (
       <div>
