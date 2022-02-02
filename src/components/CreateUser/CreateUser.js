cd97c1ae1f01e6c57571214e7c65f511cd6b910e
DevEnock <enockomondi305@gmail.com>
2022-02-02 15:38:22 +0300

    updating the cerate user component

--- src/components/CreateUser/CreateUser.js
+++ src/components/CreateUser/CreateUser.js
@@ -32,7 +32,21 @@ class CreateUser extends Component {
   render() {
     return (
       <div>
-        This is the create user component
+        <h3>Create New User</h3>
+        <form onSubmit={this.onSubmit}>
+            <div className="form-group"> 
+              <label>Username: </label>
+              <input  type="text"
+              required
+              className="form-control"
+              value={this.state.username}
+              onChange={this.onChangeUsername}
+              />
+            </div>
+            <div className="form-group">
+             <input type="submit" value="Create User" className="btn btn-primary" />
+            </div>
+        </form>
       </div>
     );
   }
