b7eca7c40e161a44b173aae5a98bcfc149b381e8
DevEnock <enockomondi305@gmail.com>
2022-02-05 23:35:13 +0300

    adding exercises to the database.

--- src/components/ExercisesList/ExercisesList.js
+++ src/components/ExercisesList/ExercisesList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import axios from 'axios';
 
 class ExercisesList extends Component {
   constructor(props){
@@ -63,6 +64,9 @@ class ExercisesList extends Component {
     }
 
     console.log(exercise);
+
+    axios.post('http:localhost:5000/exercises/add', exercise)
+          .then(res => console.log(res.data));
 
     window.location = "/"
   }
