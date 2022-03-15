69204b7711cb2fefcba2984d9499cde50b4aa258
DevEnock <enockomondi305@gmail.com>
2022-03-15 14:40:27 +0300

    redesigning navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,20 +4,21 @@ import './Navbar.css';
 
 function Navbar() {
   return( 
-    <div>
-
-      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <Link to="/" className="navbar-brand">ExcerTracker</Link>
+    <div className='navigation'>
+      <nav className="navbar">
+        <Link to="/" className="logo">
+          <img src="/img/logo.svg" alt="logo" />
+        </Link>
         <div className="collapse navbar-collapse">
-        <ul className="navbar-nav mr-auto">
-          <li className="navbar-item">
-          <Link to="/" className="nav-link">Exercises</Link>
+        <ul className="nav-container">
+          <li className="item">
+          <Link to="/" className="link">Exercises</Link>
           </li>
-          <li className="navbar-item">
-          <Link to="/create" className="nav-link">Create Exercise Log</Link>
+          <li className="item">
+          <Link to="/create" className="link">Create Exercise Log</Link>
           </li>
-          <li className="navbar-item">
-          <Link to="/user" className="nav-link">Create User</Link>
+          <li className="item">
+          <Link to="/user" className="link">Create User</Link>
           </li>
         </ul>
         </div>
