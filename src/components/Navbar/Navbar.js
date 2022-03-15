ed6be0698fb9113e574372a3caf2ffebc2899abc
DevEnock <enockomondi305@gmail.com>
2022-03-15 15:26:12 +0300

    redesigning navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,19 +9,17 @@ function Navbar() {
         <Link to="/" className="logo">
           <img src="/img/logo.svg" alt="logo" />
         </Link>
-        <div className="collapse navbar-collapse">
-        <ul className="nav-container">
-          <li className="item">
-          <Link to="/" className="link">Exercises</Link>
-          </li>
-          <li className="item">
-          <Link to="/create" className="link">Create Exercise Log</Link>
-          </li>
-          <li className="item">
-          <Link to="/user" className="link">Create User</Link>
-          </li>
-        </ul>
-        </div>
+          <ul className="nav-container">
+            <li className="item">
+              <Link to="/" className="link">Exercises</Link>
+            </li>
+            <li className="item">
+              <Link to="/create" className="link">Create Exercise</Link>
+            </li>
+            <li className="item">
+              <Link to="/user" className="link">Create User</Link>
+            </li>
+          </ul>
       </nav>
     </div>
   );
