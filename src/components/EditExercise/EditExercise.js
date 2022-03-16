cfcc8b6efd3650078c132c1d9e1afeff39a21ba5
DevEnock <enockomondi305@gmail.com>
2022-03-16 15:59:28 +0300

    code clean -up

--- src/components/EditExercise/EditExercise.js
+++ src/components/EditExercise/EditExercise.js
@@ -6,13 +6,6 @@ import "react-datepicker/dist/react-datepicker.css";
 export default class EditExercise extends Component {
   constructor(props) {
     super(props);
-
-    this.onChangeUsername = this.onChangeUsername.bind(this);
-    this.onChangeDescription = this.onChangeDescription.bind(this);
-    this.onChangeDuration = this.onChangeDuration.bind(this);
-    this.onChangeDate = this.onChangeDate.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-
     this.state = {
       username: '',
       description: '',
