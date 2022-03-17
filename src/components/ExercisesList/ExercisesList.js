0735f68e47a33fa0302a08de872f0fe5dd6870cb
DevEnock <enockomondi305@gmail.com>
2022-03-17 11:18:36 +0300

    link fix

--- src/components/ExercisesList/ExercisesList.js
+++ src/components/ExercisesList/ExercisesList.js
@@ -10,7 +10,7 @@ const Exercise = props => (
     <td>{props.exercise.duration}</td>
     <td>{props.exercise.date.substring(0,10)}</td>
     <td>
-      <Link to={"/edit/"+props.exercise._id} id="edit">edit</Link> <a href="#" onClick={() => { props.deleteExercise(props.exercise._id) }} id="delete">delete</a>
+      <Link to={`/edit/${props.exercise._id}`} id="edit">edit</Link> <a href="#" onClick={() => { props.deleteExercise(props.exercise._id) }} id="delete">delete</a>
     </td>
   </tr>
 )
