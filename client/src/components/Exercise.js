4f69e2e9fb1a9b55049b6363d4116deac785ed1e
Trend20 <enockomondi305@gmail.com>
2023-05-11 20:30:51 +0300

    adding exercise component

---
+++ client/src/components/Exercise.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const Exercise = (props) => {
+  return (
+    <div className='details'>
+    <div className='items'>
+      <h3>Assignee:</h3>
+      <p>{props.exercise.username}</p>
+    </div>
+    <div className='items'>
+     <h3>Description:</h3>
+     <p>{props.exercise.description}</p>
+    </div>
+    <div className='items'>
+      <h3>Duration:</h3>
+      <p>{props.exercise.duration}</p>
+    </div>
+    <div className='items'>
+      <h3>Due Date:</h3>
+      <p>{props.exercise.date.substring(0,10)}</p>
+    </div>
+    <div className='items'>
+      <Link to={"/edit/"+props.exercise._id} id="edit">edit</Link><a href="#" onClick={() => { props.deleteExercise(props.exercise._id) }} id="delete">delete</a>
+    </div>
+  </div>
+  )
+}
+
+export default Exercise;
